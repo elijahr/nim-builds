ff24c4c5438fe344780edd7a2b84f6252e81379c
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-22 03:28:52 -0600

    Fix clone, add debian / ubuntu support

--- build.sh
+++ build.sh
@@ -10,9 +10,13 @@ if [ "$(which apk)" != "" ]
 then
   apk add --update --no-cache git xz
   tarball="nim-${NIM_VERSION}-$(gcc -dumpmachine | sed 's/alpine-//').tar.xz"
+elif [ "$(which apt)" != "" ]
+then
+  apt-get update -q -y
+  apt-get -qq install -y git xz-utils
 fi
 
-git clone -q --depth 1 --single-branch "v${NIM_VERSION}" https://github.com/nim-lang/Nim.git
+git clone -q --depth 1 --single-branch --branch "v${NIM_VERSION}" https://github.com/nim-lang/Nim.git
 sh build_all.sh
 export PATH=/Nim/bin:$PATH
 ./koch csource -d:danger
