f1b9a9520c13b9bca599d0f82de62362b00a74fc
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-22 03:16:54 -0600

    Use build-farm

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -uex
+
+NIM_VERSION=$1
+
+tarball="nim-${NIM_VERSION}-$(gcc -dumpmachine).tar.xz"
+
+if [ "$(which apk)" != "" ]
+then
+  apk add --update --no-cache git xz
+  tarball="nim-${NIM_VERSION}-$(gcc -dumpmachine | sed 's/alpine-//').tar.xz"
+fi
+
+git clone -q --depth 1 --single-branch "v${NIM_VERSION}" https://github.com/nim-lang/Nim.git
+sh build_all.sh
+export PATH=/Nim/bin:$PATH
+./koch csource -d:danger
+./koch xz
+
+mv ./build/nim-*.tar.xz "/code/${tarball}"
+
+echo "::set-output name=tarball::${tarball}"
