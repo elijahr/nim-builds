9e20398ff4ccfe65c8bbdd08c166acb6aea70465
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-26 19:40:34 -0600

    Add debug support to script, use pixz

--- build.sh
+++ build.sh
@@ -3,44 +3,58 @@
 set -uex
 
 NIM_VERSION=$1
+DEBUG=${2:-no}
 
+if [ "$DEBUG" = "yes" ]
+then
+  extra_packages="valgrind"
+else
+  extra_packages=""
+fi
 
 if [ "$(which apk)" != "" ]
 then
-  apk add --update --no-cache wget xz
+  apk add --update --no-cache wget xz $extra_packages
+  apk add --update --no-cache pixz --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing
   tarball="nim-${NIM_VERSION}-$(gcc -dumpmachine | sed 's/alpine-//').tar.xz"
 elif [ "$(which apt)" != "" ]
 then
   apt-get update -q -y
-  apt-get -qq install -y wget xz-utils
+  apt-get -qq install -y wget xz-utils pixz $extra_packages
+elif [ "$(which pacman)" != "" ]
+then
+  pacman -Syu --noconfirm wget xz pixz $extra_packages
+  pacman -Sc --noconfirm || true
 elif [ "$(which brew)" != "" ]
 then
-  brew install wget xz
+  brew install wget xz pixz $extra_packages
 fi
 
 cd /code
 
 dir="nim-${NIM_VERSION}-$(gcc -dumpmachine)"
-tarball="${dir}.tar.xz"
 
 if [ ! -d "$dir" ]
 then
-  if [ ! -f "nim-${NIM_VERSION}.tar.xz" ]
-  then
-    wget "https://nim-lang.org/download/nim-${NIM_VERSION}.tar.xz"
-  fi
-  tar -xJf "nim-${NIM_VERSION}.tar.xz"
+  wget "https://nim-lang.org/download/nim-${NIM_VERSION}.tar.xz" -O- | pixz -d | tar x
   mv "nim-${NIM_VERSION}" "$dir"
 fi
 
 cd "$dir"
-sh build.sh --extraBuildArgs "-ggdb"
-bin/nim c koch
+if [ "$DEBUG" = "yes" ]
+then
+  sh build.sh --extraBuildArgs "-g"
+  valgrind -k bin/nim c koch
+else
+  sh build.sh
+  bin/nim c koch
+fi
 ./koch boot -d:release
 ./koch tools
 cd -
 
-tar -cJf "$tarball" "$dir"
+tarball="${dir}.tar.xz"
+tar -c "$dir" | pixz > "$tarball"
 
 echo "::set-output name=asset_name::${tarball}"
 echo "::set-output name=asset_path::${PWD}/${tarball}"
