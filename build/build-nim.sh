58d0bbbe4832a32f791f66758e3f8f9cc8795f71
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-27 02:30:48 -0600

    Run tests

--- build/build-nim.sh
+++ build/build-nim.sh
@@ -6,14 +6,14 @@ NIM_DIR=$1
 
 if [ "$(which apk)" != "" ]
 then
-  apk add --update --no-cache wget xz
+  apk add --update --no-cache wget xz sfml
 elif [ "$(which apt)" != "" ]
 then
   apt-get update -q -y
-  apt-get -qq install -y wget xz-utils
+  apt-get -qq install -y wget xz-utils libcsfml-dev
 elif [ "$(which pacman)" != "" ]
 then
-  pacman -Syu --noconfirm wget xz
+  pacman -Syu --noconfirm wget xz sfml
   pacman -Sc --noconfirm || true
 elif [ "$(which brew)" != "" ]
 then
@@ -25,5 +25,6 @@ sh build.sh
 bin/nim c koch
 ./koch boot -d:release
 ./koch tools
+./koch test
 
 echo "::set-output name=asset_name::nim-${NIM_VERSION}-$(gcc -dumpmachine).tar.xz"
