02884856f0d128ecef26ac62df795fe8f8806256
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-11-22 18:13:23 -0600

    Smaller tarballs

--- scripts/build-nims-macos-arm64.sh
+++ scripts/build-nims-macos-arm64.sh
@@ -1,17 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cd build
+set -uex
 
-git clone --branch v1.0.10 --depth 1 https://github.com/nim-lang/Nim.git nim-1.0.10
-git clone --branch v1.2.8 --depth 1 https://github.com/nim-lang/Nim.git nim-1.2.8
-git clone --branch v1.4.2 --depth 1 https://github.com/nim-lang/Nim.git nim-1.4.2
+dir="$(realpath $(dirname "${BASH_SOURCE[0]}")/..)"
 
-git clone -q --depth 1 https://github.com/nim-lang/csources_v1.git csources
+rm -rf "$dir/build/tmp"
+mkdir -p "$dir/build/tmp"
+git clone -q --depth 1 https://github.com/nim-lang/csources_v1.git "$dir/build/tmp/csources"
 
-cp -R csources nim-1.0.10/
-cp -R csources nim-1.2.8/
-cp -R csources nim-1.4.2/
+versions=("1.2.12" "1.4.8" "1.6.0")
 
-tar cvJf nim-1.0.10--arm64-macos-bigsur.tar.xz --exclude ".*" --exclude "./csources" --exclude "./nimcache" nim-1.0.10
-tar cvJf nim-1.2.8--arm64-macos-bigsur.tar.xz --exclude ".*" --exclude "./csources" --exclude "./nimcache" nim-1.2.8
-tar cvJf nim-1.4.2--arm64-macos-bigsur.tar.xz --exclude ".*" --exclude "./csources" --exclude "./nimcache" nim-1.4.2
+for version in "${versions[@]}"
+do
+  cd "$dir/build/tmp"
+  git clone --branch "v$version" --depth 1 https://github.com/nim-lang/Nim.git "nim-$version"
+  cd "nim-$version"
+  cp -R "$dir/build/tmp/csources" .
+  sh build_all.sh
+  ./koch boot --parallelBuild:8 -d:release
+  ./koch tools --parallelBuild:8 -d:release
+  ./koch nimble --parallelBuild:8 -d:release
+  cd "$dir/build/tmp"
+  tar cvJf "nim-$version--arm64-macos-bigsur.tar.xz" \
+    --exclude ".*" \
+    --exclude "nim-$version/c_code" \
+    --exclude "nim-$version/csources" \
+    --exclude "nim-$version/csources/c_code" \
+    --exclude "nim-$version/csources_v1" \
+    --exclude "nim-$version/csources_v1/c_code" \
+    --exclude "nim-$version/nimcache" \
+    "nim-$version"
+  mv "nim-$version--arm64-macos-bigsur.tar.xz" "$dir/build/"
+done
